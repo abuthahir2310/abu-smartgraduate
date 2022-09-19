2c4d6b1ecea87be37b09f1f66bc14a3b7a14f7b3
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-09-19 20:33:32 +0530

    Add files via upload

--- nchelloworld.c
+++ nchelloworld.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
-
-int main(){
-printf("hello and welcome to golden touch smart graduate program to the new college....");
- int age=40;
- char name[10]="abu";
- printf("\nMy name is....:%s",name);
- printf("\tMy age is.....:%d",age);
- char c='a';
- char cc=100;
- const double pi=3.14;
- //pi=50;
- printf("\tMy pi value is...:%lf",pi);
- printf("\n the size of pi variable is....:%d",sizeof(pi));
+int main()
+{
+    int testInteger;
+    printf("Enter an Integer:");
+    scanf("%d",&testInteger);
+    printf("number=%d",testInteger);
+    return 0;
 }

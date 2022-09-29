16fe4cbcce48400a567b2001808d328a57d01a1a
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-09-29 10:53:31 +0530

    Add files via upload

--- nchelloworld2.c
+++ nchelloworld2.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    float number1=13.5;
-    double number2=12.4;
-    printf("number1 =%f\n",number1);
-    printf("number2 =%lf",number2);
+    char c='a';
+    printf("character=%c",c);
     return 0;
 }

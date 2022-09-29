dfa3339a1f622d1480d6cd9f0b20fd81125fad25
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-09-29 12:43:38 +0530

    Add files via upload

---
+++ nchelloworld3.2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("enter the number:\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("you entered=%d\n",c);
+    return 0;
+}

4dc6568a4a16560fe97f38db7ad91d86923c1b7b
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-06 20:08:40 +0530

    Add files via upload

---
+++ QuotientRemaider.java
@@ -0,0 +1,12 @@
+
+public class QuotientRemaider {
+
+	public static void main(String[] args) {
+		int divided=25,dividor=4;
+		int Quotient=divided/dividor;
+		int remainder=divided%dividor;
+		System.out.println("Quotient="+Quotient);
+		System.out.println("remainder="+remainder);
+	}
+
+}

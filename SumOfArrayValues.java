7c1c427fa802302d303a35a4c52d1127f81a544d
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-07 19:08:52 +0530

    Add files via upload

---
+++ SumOfArrayValues.java
@@ -0,0 +1,13 @@
+
+public class SumOfArrayValues {
+
+	public static void main(String[] args) {
+		int[] array= {10,20,30,04,50,10};
+		int Sum=0;
+		for(int num:array) {
+			Sum= Sum+num;
+		}
+		System.out.println("sum of array elements is:"+Sum);
+	}
+
+}

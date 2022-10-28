e5b4efea5713a2dde8ecc1e3dbc3b75dee53fa6f
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-28 20:36:50 +0530

    Add files via upload

---
+++ PrintDayInWorldSwitch.java
@@ -0,0 +1,38 @@
+package PartiallyClosedLabBook;
+
+public class PrintDayInWorldSwitch {
+
+	public static void main(String[] args) {
+		int day=6;
+		switch(day){
+			case 1:
+				System.out.println("It is Sunday");
+				break;
+			case 2:
+				System.out.println("It is Monday");
+				break;
+			case 3:
+				System.out.println("It is Tuesday");
+				break;
+			case 4:
+				System.out.println("It is Wednesday");
+				break;
+			case 5:
+				System.out.println("It is Thursday");
+				break;
+			case 6:
+				System.out.println("It is Friday");
+				break;
+			case 7:
+				System.out.println("It is Saturday");
+				break;
+			default:
+			System.out.println("Input is Correct");
+		}
+		
+		
+		
+
+	}
+
+}

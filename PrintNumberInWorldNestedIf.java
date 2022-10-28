8a17a48eca90237fc357b0532646b8123606bb52
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-28 18:32:49 +0530

    Add files via upload

---
+++ PrintNumberInWorldNestedIf.java
@@ -0,0 +1,28 @@
+package PartiallyOpenedLabBooks;
+
+public class PrintNumberInWorldNestedIf {
+
+	public static void main(String[] args) {
+		int number=5;
+		if(number==1) {
+			System.out.println("1");
+		}
+		else if(number==2){
+			System.out.println("2");
+		}
+		else if(number==3) {
+			System.out.println("3");
+		}
+		else if(number==4) {
+			System.out.println("4");
+		}
+		else if(number==5) {
+			System.out.println("5");
+		}
+		else {
+			System.out.println("Done");
+		}
+
+	}
+
+}

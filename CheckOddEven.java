9c6dee25cb6a5c058bf379f62d4f7d9ba6a2336b
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-28 18:21:03 +0530

    Add files via upload

---
+++ CheckOddEven.java
@@ -0,0 +1,17 @@
+package PartiallyOpenedLabBooks;
+
+public class CheckOddEven {
+
+	public static void main(String[] args) {
+		int number=50;
+		System.out.println("The number is:"+number);
+		if(number%2==0) {
+			System.out.println("Even number");
+		}
+		else {
+			System.out.println("Odd number");
+		}
+        System.out.println("Done");
+	}
+
+}

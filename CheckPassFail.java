fbcc3ccf2c85e392e7715bff9a9854134dde8f91
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-28 18:08:43 +0530

    Add files via upload

---
+++ CheckPassFail.java
@@ -0,0 +1,17 @@
+
+public class CheckPassFail {
+
+	public static void main(String[] args) {
+		int mark=49;
+		System.out.println("The mark is:"+mark);
+		if(mark>50) {
+			System.out.println("FAIL");
+		}
+		else {
+			System.out.println("PASS");
+		}
+		System.out.println("ITS OVER");
+
+	}
+
+}

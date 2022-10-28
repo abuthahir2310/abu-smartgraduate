fd2be00dcabcb75a03cb20d0cdb4517705f2e5e6
abuthahir2310 <113002035+abuthahir2310@users.noreply.github.com>
2022-10-28 18:51:12 +0530

    Add files via upload

---
+++ PrintDayInWorldIfElse.java
@@ -0,0 +1,34 @@
+package PartiallyOpenedLabBooks;
+
+public class PrintDayInWorldIfElse {
+
+	public static void main(String[] args) {
+	     int day=5;
+	     if(day==1) {
+	    	 System.out.println("It is Sunday");
+	     }
+	     else if(day==2) {
+	    	 System.out.println("It is Monday");
+	     }
+	     else if(day==3) {
+	    	 System.out.println("It is Tuesday");
+	     }
+	     else if(day==4) {
+	    	 System.out.println("It is Wednesday");
+	     }
+	     else if(day==5) {
+	    	 System.out.println("It is Thursday");
+	     }
+	     else if(day==6) {
+	    	 System.out.println("It is Friday");
+	     }
+	     else if(day==7) {
+	    	 System.out.println("It is Saturday");
+	     }
+	     else {
+	    	 System.out.println("INPUT IS CORRECT" );
+	     }
+
+	}
+
+}
